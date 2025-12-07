de7756fe3092d88ef0bd62e871ef705de20e93d5
syarlfaaa <149407232+syarlfaaa@users.noreply.github.com>
2025-12-07 15:58:35 +0700

    config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-# Configuration settings
+#i built the Gradio in colab and hosted it after that (using a dataset to detect skin types)
 
-GRADIO_URL = "https://huggingface.co/spaces/whocataleya/ai-skin-detector"
+GRADIO_URL = "https://huggingface.co/spaces/whocataleya/ai-skin-detector" 
 
 USERS_FILE = "users.json"
 
-# Email configuration
-GMAIL_USER = "syarifakhoirunisa6@gmail.com"
-GMAIL_PASSWORD = "fblv khcs rfht msru"
+#turn on 2 step verification in ur gmail security settings >> get the 16 char code >> done
+GMAIL_USER = "urgmail6@gmail.com"
+GMAIL_PASSWORD = "1234 5678 9123"
 
 # OTP storage (in-memory)
 OTP_STORE = {}
